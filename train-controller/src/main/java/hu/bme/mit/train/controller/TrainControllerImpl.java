96d62672c65e43f20592bc5a62a1a1609f140dbb
Stark-Morucz <nandi9801@gmail.com>
2020-03-28 20:46:38 +0100

    +task 1 implemented

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -5,13 +5,30 @@ import com.google.common.collect.Table;
 import hu.bme.mit.train.interfaces.TrainController;
 
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
-	private int speedLimit = 100;
+	private int speedLimit = 99; //in order to make defense from Covid-19
 	public Table<String, String, Long> tachograph = HashBasedTable.create();
+	private Thread t; //i ve chosen thread as the descreption of task suggested
+	private int interval = 3000; //3sec periodic
+
+	public TrainControllerImpl() {
+		t = new Thread() {
+			public void run() {
+				t.run();
+				try {
+					followSpeed();
+					t.sleep(interval);
+				}
+				catch (Exception err) { err.printStackTrace(); }
+			}
+		};
+	}
 
 	public void recordData() {
 		String cTime = new Date().toString();
